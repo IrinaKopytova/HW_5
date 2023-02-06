2f638f1fef9932bad9f1fecfff44a4079f29d90d
Irina <kopytova.irina@inbox.ru>
2023-02-06 16:40:36 +0300

    HW_5

---
+++ task_36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArrayWithRandom(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+int[] array = new int [5];
+FillArrayWithRandom(array);
+Console.Write("Ваш случайный массив: ");
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine("Сумма элементов: " + sum);
---
+++ task_38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.7; 7.2; 2.1; 7.8] -> 5.7
+
+double[] FillArrayWithRandom(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToDouble(new Random().Next(0, 100)) / 10;
+    }
+    return array;
+}
+
+double[] array = new double [5];
+FillArrayWithRandom(array);
+Console.Write("Ваш случайный массив: ");
+Console.Write(string.Join("; ", array));
+Console.WriteLine();
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+double diff = max - min;
+Console.WriteLine("Максимальное значение  = " + max);
+Console.WriteLine("Минимальное значение = " + min);
+Console.WriteLine("Разница между max и min = " + diff);
